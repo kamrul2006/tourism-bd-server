0b653e4ffaf3aea8fe93f4b99b75a9a59805c659
Kamrul Islam Apurba <kamrulislamapurba@gmail.com>
2026-02-07 23:14:28 +0600

    Bassic updated 4

--- index.js
+++ index.js
@@ -9,20 +9,17 @@ const port = process.env.PORT || 5000
 const app = express();
 
 // -----------middleware-----
-app.use(cors(
-    {
-        origin: [
-            'http://localhost:5173',
-            'http://localhost:5173',
-            'https://tourismbd-by-k-web-tech.firebaseapp.com',
-            'https://tourismbd-by-k-web-tech.web.app'
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'https://tourismbd-by-k-web-tech.firebaseapp.com',
+        'https://tourismbd-by-k-web-tech.web.app'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 
-        ],
-        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
-        credentials: true
-    }
-))
 app.use(express.json())
 app.use(cookieParser());
 
